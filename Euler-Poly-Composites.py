3dd39f08b855fa8496f5c4f747533ecf07ce1c7c
Manuel Herrera <mherrera@csus.edu>
2019-10-01 14:23:40 -0700

    second polynomial

--- Euler-Poly-Composites.py
+++ Euler-Poly-Composites.py
@@ -12,9 +12,18 @@ def isComposite(n):
         i = i + 6
     return False
     
+print('First polynomial h(n) = 103n^2-3945n+34381')
 i = 0
 while (i < 100):
     x = (103 * (pow(i, 2))) - (3945 * i) + 34381
     if isComposite(x):
         print("Integer {} : {}".format(i, x))
     i += 1
+
+print('Second Polynomial k(n) = 36n^2-810n+2753')
+j = 0
+while (j < 100):
+    y = (36 * (pow(j, 2))) - (810 * j) + 2753
+    if isComposite(y):
+        print("Integer {} : {}".format(j, y))
+    j += 1
